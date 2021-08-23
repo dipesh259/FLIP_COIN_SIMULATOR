48be0fc5c26d1f24b42d30505762ef60e020df57
sick3356 <myselfdipu259@gmail.com>
2021-08-23 12:56:41 +0530

    multiple times

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,15 +1,37 @@
 #!/bin/bash 
 
-#CONSTANT
-
 HEAD=0
+headCount=0
+tailCount=0
 
-#TO PRINT THE HEAD AND TAIL
+#TO DECLARE DICTIONARY
 
-flipCoin=$((RANDOM%2))
-if [ $flipCoin == $HEAD ]
-then
-	echo "Head"
-else
-	echo "Tail"
-fi
+declare -A singletFlip
+
+#USER INPUT 
+
+read -p "Enter the Number of Coin Flip : " numberOfCoinFlip
+
+#TO STORE HEAD COUNT AND TAIL COUNT IN DICTIONARY
+
+for(( count=0; count<$numberOfCoinFlip; count++ ))
+do
+   FlipCoin=$(( RANDOM % 2 ))
+
+   if [ $FlipCoin -eq $HEAD ]
+   then
+      singletFlip[HEAD]=$((++headCount))
+   else
+      singletFlip[TAIL]=$((++tailCount))
+   fi
+done
+
+#TO PERCENTAGE
+
+singletHeadPercentage=`echo "scale=2; $headCount * 100 / $numberOfCoinFlip" | bc`
+singletTailPercentage=`echo "scale=2; $tailCount *100 / $numberOfCoinFlip" | bc`
+
+#TO PRINT
+
+echo "To single head percentage and head count:$headCount : " $singletHeadPercentage
+echo "To single tail percentage and tail count:$tailCount : " $singletTailPercentage
